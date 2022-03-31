3818f6a72247adc0dcc340438b0315aec9113bb2
shashikiranshastry <91182428+shashikiranshastry@users.noreply.github.com>
2022-03-31 11:11:07 +0530

    Update test_tdd_buckets.py

--- test_tdd_buckets.py
+++ test_tdd_buckets.py
@@ -1,10 +1,23 @@
 import unittest
-import tdd_buckets
+import chargeRangeReadings
 
-class FrequentRangeTest(unittest.TestCase):
-    def test_get_frequent_range(self):
-        readings = [1,1,3,2]
-        self.assertTrue(frequent_range.get_frequent_range(readings) == '1-3, 4')
+class chargeRangeReadingsTest(unittest.TestCase):
+  
+  def test(self):
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([], chargeRangeReadings.ADC_12Bit) == "")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0], chargeRangeReadings.ADC_12Bit) == "0-0, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094,4095], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")#ignore error readings
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,4000], chargeRangeReadings.ADC_12Bit) == "0-0, 1\n10-10, 1\n")
+    
+    
+    
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([], chargeRangeReadings.ADC_10Bit) == "")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([511], chargeRangeReadings.ADC_10Bit) == "0-0, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022,1023], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")#ignore error readings
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,100,150,300,500,70,1500,700,600,2000,1022], chargeRangeReadings.ADC_10Bit) == "0-0, 1\n3-3, 1\n6-6, 2\n11-13, 3\n15-15, 2\n")
 
-if __name__ == '__main__':
-  unittest.main()
+    
+unittest.main()
